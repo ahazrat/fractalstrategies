512e6061c2e68c91051a69da33f5fafa52f32648
Asif Hazrat <asifhazrat@gmail.com>
2015-09-24 00:58:47 +0000

    add data folder, add views folder, add routes, add view engine

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+// set variables for environment
 var express = require('express');
 var app = express();
+var path = require('path');
 
-var app1 = angular.module('app', []);
+// set views engine and root folder
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
 
-app1.directive('hello', [function() {
-    return {
-        restrict: 'CEMA',       // C: class, E: element, M: comments, A: attributes
-        replace: true,          // replaces original content with template
-        template: '<span><br>Hello</span>'
-    }
-}]);
+// set routes
+app.get('/', function(req, res) {
+    res.render('index.');
+});
 
 app.listen(process.env.PORT, process.env.IP);
 console.log('Asif: The <fractalStrategies> server is running.');
