b807731ebad241181979fcb73fd27ab018ab3fc8
Asif Hazrat <asifhazrat@gmail.com>
2015-09-24 00:37:47 +0000

    add express to app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-var app = angular.module('app', []);
+var express = require('express');
+var app = express();
 
-app.directive('hello', [function() {
+var app1 = angular.module('app', []);
+
+app1.directive('hello', [function() {
     return {
         restrict: 'CEMA',       // C: class, E: element, M: comments, A: attributes
         replace: true,          // replaces original content with template
         template: '<span><br>Hello</span>'
     }
 }]);
+
+app.listen(process.env.PORT, process.env.IP);
+console.log('Asif: The <fractalStrategies> server is running.');
